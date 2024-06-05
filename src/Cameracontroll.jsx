323994bf64bc8c519a599719e67b13fa569c308a
Danyal494 <dk7167301@gmail.com>
2024-06-05 05:59:27 -0700

    secound commit

--- src/Cameracontroll.jsx
+++ src/Cameracontroll.jsx
@@ -1,11 +1,12 @@
 import { Environment, OrbitControls, RandomizedLight } from '@react-three/drei'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const CameraController = () => {
     const { camera } = useThree()
     const horizontalAngleRef = useRef(0)
     const verticalAngleRef = useRef(0)
+    const [lastTouch, setLastTouch] = useState({ x: 0, y: 0 })
 
     useEffect(() => {
         const handleScroll = (event) => {
@@ -13,9 +14,30 @@ const CameraController = () => {
             verticalAngleRef.current += event.deltaY * 0.001
         }
 
+        const handleTouchStart = (event) => {
+            const touch = event.touches[0]
+            setLastTouch({ x: touch.clientX, y: touch.clientY })
+        }
+
+        const handleTouchMove = (event) => {
+            const touch = event.touches[0]
+            const deltaX = touch.clientX - lastTouch.x
+            const deltaY = touch.clientY - lastTouch.y
+            horizontalAngleRef.current += deltaX * 0.001
+            verticalAngleRef.current += deltaY * 0.001
+            setLastTouch({ x: touch.clientX, y: touch.clientY })
+        }
+
         window.addEventListener('wheel', handleScroll, { passive: true })
-        return () => window.removeEventListener('wheel', handleScroll)
-    }, [])
+        window.addEventListener('touchstart', handleTouchStart, { passive: true })
+        window.addEventListener('touchmove', handleTouchMove, { passive: true })
+
+        return () => {
+            window.removeEventListener('wheel', handleScroll)
+            window.removeEventListener('touchstart', handleTouchStart)
+            window.removeEventListener('touchmove', handleTouchMove)
+        }
+    }, [lastTouch])
 
     useFrame(() => {
         const radius = 5
